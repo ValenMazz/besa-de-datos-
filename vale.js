2be63faef2793ce0d5f4be61959125be3380f3c0
alumno <alumno@Instituto.local>
2025-10-29 11:12:36 -0300

    ...

--- vale.js
+++
@@ -1,8 +0,0 @@
-
-    apiKey: "AIzaSyBRIj6XV-ODhvEz5RUo4bLRWmWOzgP2Ce8",
-    authDomain: "basededatos-16bad.firebaseapp.com",
-    projectId: "basededatos-16bad",
-    storageBucket: "basededatos-16bad.firebasestorage.app",
-    messagingSenderId: "568461938141",
-    appId: "1:568461938141:web:141767f08d69d06a183a34"
- 
